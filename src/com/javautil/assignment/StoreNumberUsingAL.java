5be784c18e6e02894d3cc8bd4c0ccc564971bf5a
Govind_ <Govind_@Govind>
2019-03-16 01:00:27 +0530

    Commit Assignment1 from java.util.example

---
+++ src/com/javautil/assignment/StoreNumberUsingAL.java
@@ -0,0 +1,17 @@
+package com.javautil.assignment;
+
+import java.util.ArrayList;
+
+public class StoreNumberUsingAL {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Number> al = new ArrayList<Number>();
+		
+		al.add(10);
+		al.add(15.0f);
+		al.add(10.0d);
+	//	al.add("govind"); CE
+		System.out.println(al);
+	}
+}
